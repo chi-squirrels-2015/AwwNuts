42e67c95fe9dbfe9976d59885b9d4b9c6db6469c
John Schulz <johnschulz75@gmail.com>
2015-02-26 16:43:07 -0600

    Fixing Comment Specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -10,9 +10,9 @@ describe Comment do
 			expect{ comment.save }.to_not change { Comment.count }
 		end
 	end
-	# let(:author)  { User.create(id: 1, username: "Nate", email: "nate@dbc.com", password_digest: "password") }
+	let(:author)  { User.create(id: 1, username: "Nate", email: "nate@dbc.com", password_digest: "password") }
 	let(:answer)  { Question.create(id: 1, title: "Test Title", content: "Fake answer content", author_id: 1, )}
-	let(:comment) { Comment.new( content: "Fake comment", commentable_type: "Question", commentable_id: 1) }
+	let(:comment) { Comment.new( author: author, content: "Fake comment", commentable_type: "Question", commentable_id: 1) }
 
 	it 'should have content' do
 		expect(comment.content). to eq("Fake comment")
@@ -26,7 +26,7 @@ describe Comment do
 		expect(comment.commentable_id). to eq(1)
 	end
 
-	xit 'should have an author' do
+	it 'should have an author' do
 		expect(comment.author.username). to eq("Nate")
 	end
 end
