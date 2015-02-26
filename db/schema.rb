788f14f83bc29d5f81c8efe73b75a2a04470a47a
Matthew Gray <graymatthew3@gmail.com>
2015-02-26 16:53:24 -0600

    fix some broken tests nd typos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema.define(version: 20150226193309) do
+
+ActiveRecord::Schema.define(version: 20150226194138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
