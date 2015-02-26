eb1d062c2a1a4c6e0e4404d237649a443b027ebd
aamharris <aamharris@gmail.com>
2015-02-26 17:59:38 -0600

    add initial controller routes and actions for questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,51 @@
+class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.build(question_params)
+  
+    if @question.save
+      redirect_to question_path(@question)
+    else
+      @error = @question.errors
+      render :new
+    end
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(question_params)
+      redirect_to question_path(@question)
+    else
+      @error = @question.errors
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.delete
+    redirect_to questions_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,14 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   resources :users, only: [:new, :create]
-  resources :questions, only: [:new, :create, :edit, :update, :destroy] do
+
+  resources :questions do
     resources :comments, only: [:new, :create]
     resources :answers, only: [:new, :create, :edit, :update, :destroy] do
       resources :comments, only: [:new, :create]
     end
   end
+
 
   get "/dashboard" => 'users#show'
 
