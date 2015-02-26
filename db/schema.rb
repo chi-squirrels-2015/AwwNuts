5d2e6d94148d930edfe073dcb92a5e7d1faf19c3
John Schulz <johnschulz75@gmail.com>
2015-02-26 15:01:17 -0600

    Comment Model Tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226192403) do
+ActiveRecord::Schema.define(version: 20150226193309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "author_id"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer  "author_id"
@@ -23,6 +31,14 @@ ActiveRecord::Schema.define(version: 20150226192403) do
     t.integer  "commentable_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "author_id"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- spec/spec_comment.rb
+++ spec/spec_comment.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe Comment do
-	let(:author)  { User.create(id: 1, username: "Nate", email: "nate@dbc.com", password_digest: "password") }
+	# let(:author)  { User.create(id: 1, username: "Nate", email: "nate@dbc.com", password_digest: "password") }
 	let(:answer)  { Question.create(id: 1, title: "Test Title", content: "Fake answer content", author_id: 1, )}
-	let(:comment) { Comment.create( author_id: 1, content: "Fake comment", commentable_type: "Question", commmentable_id: 1) }
+	let(:comment) { Comment.create( author_id: 1, content: "Fake comment", commentable_type: "Question", commentable_id: 1) }
 
-	it 'should have an author' do
+	it 'should have content' do
+		expect(comment.content). to eq("Fake comment")
+	end
+
+	it 'should have a commentable_type' do
+		expect(comment.commentable_type). to eq("Question")
+	end
+
+	it 'should have a commentable_id' do
+		expect(comment.commentable_id). to eq(1)
+	end
+
+	xit 'should have an author' do
 		expect(comment.author.username). to eq("Nate")
 	end
 end
