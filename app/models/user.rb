f1977962b4ed595f5c6c961e3e123559d9f310e6
Matthew Gray <graymatthew3@gmail.com>
2015-02-27 14:46:43 -0600

    add #avatar_url default getter

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,14 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: /\w+@\w+\.\w+/i }
-  validates :avatar_url, format: { with: /\Ahttps?:\/\//i }
+  validates :avatar_url, format: { with: /\Ahttps?:\/\//i }, allow_blank: true
 
   def full_name
     "#{first_name} #{last_name}".titleize
+  end
+
+  def avatar_url
+    @avatar_url ||= "http://movingkidsforward.org/wp-content/uploads/2013/05/profile_default800x600.jpg"
   end
 
   private
