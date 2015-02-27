0ba2ea80e08137e2cda1c9932e820efde1667955
bsheridan12 <bsheridan12@gmail.com>
2015-02-27 10:45:03 -0600

    add start to answers controller spec

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe AnswersController do
+	# let!(:jean) {User.create!(username: "Jean Luc Picard", password: "test" )}
+	# let(:bill) {User.new(username: "Bill Gates", email: "thebill@gmail.com", password: "ilovewindows")}
+
+	# let(:question) {Question.new(title: "How do I install Windows?", content: "I have no idea, I need help", author: bill)}
+	# let!(:answer) { Answer.create!(content: "Make it work.", author: jean)} 
+
+	# describe "GET #new" do
+	# 	it "assigns renders a create new instance of answer" do
+	# 		get :new
+	# 		expect(assigns(:answer)).to be_instance_of Answer
+	# 		expect(assigns(:answer).id).to be_nill
+	# 	end
+	# end
+
+end
