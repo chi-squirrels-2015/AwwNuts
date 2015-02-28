d894c5421c9a8cf26b4a8809edb88f32d0fde2af
Matthew Gray <graymatthew3@gmail.com>
2015-02-27 18:45:12 -0600

    add html/css for question show and index, update root to questions#index

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root 'users#new'
+  root 'questions#index'
 
   resources :users, only: [:new, :create]
 
