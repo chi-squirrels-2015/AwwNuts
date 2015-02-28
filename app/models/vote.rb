1d2a69fb9658dd35a56be86186f199867b3fb547
Matthew Gray <graymatthew3@gmail.com>
2015-02-28 09:35:55 -0600

    test vote model, add validations, add #up and #down

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,10 +1,21 @@
 class Vote < ActiveRecord::Base
-
-  validates_uniqueness_of :voter, scope: [:votable_id, :votable_type]
+  validates :voter, presence: true, uniqueness: { scope: [:votable_id, :votable_type] }
+  validates :votable, presence: true
+  validates :votable_type, inclusion: { in: ["Question", "Answer"] }
   validates_inclusion_of :count, :in => (-1..1)
-  validates_presence_of :votable
 
   belongs_to :votable, polymorphic: true
   belongs_to :voter, class_name: "User"
 
+  def up
+    if count == -1 || count == 0
+      self.count += 1
+    end
+  end
+
+  def down
+    if count == 1 || count == 0
+      self.count -= 1
+    end
+  end
 end
