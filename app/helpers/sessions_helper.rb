153e36ff64d2e82c272bd157bf555bdb783ce968
aamharris <aamharris@gmail.com>
2015-02-27 19:58:52 -0600

    add user variables for dashboard

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,28 @@
 module SessionsHelper
+  require 'Date'
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def member_for
+    start_time = DateTime.now
+    end_time = current_user.created_at
+    distance_of_time_in_words(start_time, end_time)
+  end
+
+  # def activity_stream(user)
+  #   activities = []
+  #   Question.find(user).order_by(created_at)
+  #   Answer.find(user).order_by(created_at)
+  #   Comment.find(user).order_by(created_at)
+  # end
+
+  def question_activity(id)
+    question = Question.find(id)
+    start_time = DateTime.now
+    end_time = question.created_at
+    distance_of_time_in_words(start_time, end_time)
   end
 
 end
